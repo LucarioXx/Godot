89390b12882402aff6e20376cfbc6b91e446b5d4
CrazyGamesPublic <crazygamesoffical@gmail.com>
2025-10-16 00:38:34 +0200

    charakter

--- test-123/Scripts/player.gd
+++ test-123/Scripts/player.gd
@@ -1,20 +1,51 @@
 extends CharacterBody2D
 
 const SPEED = 50
+var cdirection = "none"
+
+
+var last_dir = Vector2(0, 1)  # Standard: nach unten schauen
 
 func _physics_process(delta):
-	var input_vector = Vector2.ZERO
+	var input_vector = Vector2(
+		Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left"),
+		Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
+		
+	)
 
-	# Eingaben abfragen (horizontal & vertikal)
-	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
-	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
-
-	# Wenn eine Richtung gedrückt ist → normalisieren (damit diagonale Bewegung nicht schneller ist)
 	if input_vector != Vector2.ZERO:
 		input_vector = input_vector.normalized()
+		velocity = input_vector * SPEED
+		animation_player(input_vector)
+		print(input_vector)
+		last_dir = input_vector  # Merke die letzte Bewegungsrichtung
+	else:
+		velocity = Vector2.ZERO
+		$AnimatedSprite2D.play("front_idle")
+		
 
-	# Geschwindigkeit berechnen
-	velocity = input_vector * SPEED
-
-	# Bewegung ausführen
 	move_and_slide()
+
+func animation_player(inputvalue):
+	var anim= $AnimatedSprite2D
+
+	if inputvalue.x > 0 and inputvalue.y == 0:
+		anim.flip_h = false
+		anim.play("side_walk")
+	elif inputvalue.x > 0 and inputvalue.y < 0:
+		anim.flip_h = false
+		anim.play("side_walk")
+	elif inputvalue.x > 0 and inputvalue.y > 0:
+		anim.flip_h = false
+		anim.play("side_walk")
+	elif inputvalue.x < 0 and inputvalue.y == 0:
+		anim.flip_h = true
+		anim.play("side_walk")
+	elif inputvalue.x < 0 and inputvalue.y < 0:
+		anim.flip_h = true
+		anim.play("side_walk")
+	elif inputvalue.x < 0 and inputvalue.y > 0:
+		anim.flip_h = true
+		anim.play("side_walk")
+
+	
